56f3151daae70a9aef824c3fa1150b6e6ac78adb
surendra dj h <98631400+Surendradjh@users.noreply.github.com>
2025-04-09 19:12:33 +0530

    Update EDA.py

--- Deployment/pages/EDA.py
+++ Deployment/pages/EDA.py
@@ -40,19 +40,19 @@ if a=="Single Feature analysis":
 
     - The maximum age is 95-98""")
 
-    st.image(r"Deployment\plots\count based on Gender.png")
+    st.image(r"Deployment/plots/count based on Gender.png")
     st.write("Half of the participants in this dataset are male, while the other half include females and individuals of other gender identities.")
-    st.image(r"Deployment\plots\sleeping Hours.png")
+    st.image(r"Deployment/plots/sleeping Hours.png")
     st.write("Here the Sleeping hours are following the normal distribution ")
-    st.image(r"Deployment\plots\Social Interaction.png")
+    st.image(r"Deployment/plots/Social Interaction.png")
     st.write("People's social Interaction is almost following the uniform distribution")
-    st.image(r"Deployment\plots\Physical Activity.png")
+    st.image(r"Deployment/plots/Physical Activity.png")
     st.write("People's Pysical activity timing's are following the uniform distribution")
 else:
     st.subheader("multiple feature's analysis ")
-    st.image(r"Deployment\plots\Age vs Treatment.png")
+    st.image(r"Deployment/plots/Age vs Treatment.png")
     st.write("Older age groups show a significantly higher count of individuals seeking treatment, while the group not requiring treatment appears steady and much smaller in comparison.")
-    st.image(r"Deployment\plots\Data Distributions.png")
+    st.image(r"Deployment/plots/Data Distributions.png")
     st.write("""
             - ✅ No significant outliers are observed across the features, so there’s no immediate need for outlier handling techniques like trimming or winsorization.
              
@@ -60,7 +60,7 @@ else:
 
             - Physical Activity has a wide spread, showing high variability. This suggests people have very different levels of physical activity, which might be an important factor to consider in the analysis.""")
 
-    st.image(r"Deployment\plots\Gender-Diet-Treatment.png")
+    st.image(r"Deployment/plots/Gender-Diet-Treatment.png")
     st.write("""
     - 👨‍⚕️ Males with poor diet quality represent the largest group seeking treatment, highlighting a strong link between poor nutrition and mental health issues.
 
@@ -69,6 +69,6 @@ else:
     - ✅ This trend strongly suggests that diet quality is a key factor influencing mental health, and maintaining a healthy diet may reduce the likelihood of needing mental health treatment.
 
 """)
-    st.image(r"Deployment\plots\Family History vs Treatment.png")
+    st.image(r"Deployment/plots/Family History vs Treatment.png")
     st.write("The plot suggests no strong relationship between family history and treatment, as many individuals without a family history still seek mental health support.")
 
